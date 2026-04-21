b7ae08740aaac8106d9458d0b8c11bc40665424d
Ricky <w_ziwei2004@163.com>
2026-04-21 13:48:01 +0800

    static_lib

---
+++ src/link/static_lib.bat
@@ -0,0 +1,8 @@
+gcc -c addvec.c -o static_lib/addvec.o
+gcc -c multvec.c -o static_lib/multvec.o
+
+ar rcs static_lib/libvector.a static_lib/addvec.o static_lib/multvec.o
+
+gcc -c main.c -o static_lib/main.o
+
+gcc -static -o static_lib/prog static_lib/main.o static_lib/libvector.a
